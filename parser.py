aa9d63c81a35cb042aec76e301239658d847f6b1
M4ndU <33446356+M4ndU@users.noreply.github.com>
2018-02-19 19:39:58 +0900

    new

--- parser.py
+++ parser.py
@@ -14,26 +14,33 @@ def get_html(url):
 def get_diet(code, ymd, weekday):
     schMmealScCode = code #int
     schYmd = ymd #str
-    num = weekday + 1 #int 0월1화2수3목4금5토6일
-    URL = (
-            "http://stu.sen.go.kr/sts_sci_md01_001.do?"
-            "schulCode=B100000519&schulCrseScCode=4&schulKndScCode=04"
-            "&schMmealScCode=%d&schYmd=%s" % (schMmealScCode, schYmd)
-        )
-    html = get_html(URL)
-    soup = BeautifulSoup(html, 'html.parser')
-    element = soup.find_all("tr")
-    element = element[2].find_all('td')
-    element = element[num] #num
-    element = str(element)
+    if weekday == 6:
+        element = " "
+    else:
+        num = weekday + 1 #int 0월1화2수3목4금5토6일
+        #URL 수정해서 사용하세요
+        URL = (
+                "http://stu.sen.go.kr/sts_sci_md01_001.do?"
+                "schulCode=B100000519&schulCrseScCode=4&schulKndScCode=04"
+                "&schMmealScCode=%d&schYmd=%s" % (schMmealScCode, schYmd)
+            )
+        html = get_html(URL)
+        soup = BeautifulSoup(html, 'html.parser')
+        element = soup.find_all("tr")
+        element = element[2].find_all('td')
+        try:
+            element = element[num] #num
+            element = str(element)
 
-    element = element.replace('[', '')
-    element = element.replace(']', '')
-    element = element.replace('<br/>', '\n')
-    element = element.replace('<td class="textC last">', '')
-    element = element.replace('<td class="textC">', '')
-    element = element.replace('</td>', '')
-    element = element.replace('(h)', '')
-    element = element.replace('.', '')
-    element = re.sub(r"\d", "", element)
+            element = element.replace('[', '')
+            element = element.replace(']', '')
+            element = element.replace('<br/>', '\n')
+            element = element.replace('<td class="textC last">', '')
+            element = element.replace('<td class="textC">', '')
+            element = element.replace('</td>', '')
+            element = element.replace('(h)', '')
+            element = element.replace('.', '')
+            element = re.sub(r"\d", "", element)
+        except:
+            element = " "
     return element
